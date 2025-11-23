46ec73d13049b80f8cfbab48a08c39ded0a06e49
karas Asharf <karasashaerfgmail.com>
2025-11-23 21:40:18 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigModule } from '@nestjs/config';
-async function bootstrap() {
-   //const app = await NestFactory.create(AppModule);
-   const app = await NestFactory.create(AppModule);
- 
-const port= process.env.PORT??5000
-   await app.listen(port,()=>{
-console.log(`Server is Running http://localhost:${port}`);
-// console.log(`Application is running on: ${ app.getUrl()}`);
-  })
-  
-}
-bootstrap();
+import { ConfigService } from '@nestjs/config';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // ممكن تحط هنا دومين فرونت معين
+    methods: 'GET,POST,PUT,DELETE,PATCH',
+  });
+
+  // استخدام ConfigService للحصول على PORT
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') ?? 5000;
+
+  await app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+bootstrap();
 
